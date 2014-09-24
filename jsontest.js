7fea549a6e0ef94bef29fec417aff0a57e021fef
Toby Hinloopen <toby@zisoo.nl>
2014-09-25 01:21:50 +0200

    fix

--- jsontest.js
+++ jsontest.js
@@ -7,25 +7,35 @@ var fs = require("fs"),
     tag = html.tag,
     starttag = html.starttag;
 
+var cachedDatastr = null;
+
 function jsontest(res) {
   var starttime = process.hrtime();
   res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-  fs.readFile("./data.json", function (err, datastr) {
-    if(err) throw err;
-    var data = JSON.parse(datastr), str = [], key;
+  if(cachedDatastr)
+    respondWithDataStr(res, cachedDatastr)
+  else
+    fs.readFile("./data.json", function (err, datastr) {
+      if(err) throw err;
+      cachedDatastr = datastr;
+      respondWithDataStr(res, datastr);
+    });
+}
 
-    str.push(doctype(),
-        tag("title", "NodeJS JSON + I/O test"),
-        style(),
-        menu(res));
+function respondWithDataStr(res, datastr) {
+  var data = JSON.parse(datastr), str = [], key;
 
-    for(key in data)
-      str.push(tag("code", key + ": " + data[key]),
-          starttag("br"));
+  str.push(doctype(),
+      tag("title", "NodeJS JSON + I/O test"),
+      style(),
+      menu(res));
 
-    str.push(footer(starttime));
-    res.end(str.join(""));
-  });
+  for(key in data)
+    str.push(tag("code", key + ": " + data[key]),
+        starttag("br"));
+
+  str.push(footer(starttime));
+  res.end(str.join(""));
 }
 
 module.exports = jsontest;
